b908671a2610f05ccd67602c5acd6d393c4c4266
Kyle Krull <kkrull@8thlight.com>
2018-07-09 14:10:14 -0500

    Function wiht multiple arity

--- hello_world/lib/hello_world.ex
+++ hello_world/lib/hello_world.ex
@@ -2,4 +2,8 @@ defmodule HelloWorld do
   def hello do
     "Hello World!"
   end
+
+  def hello(name) do
+    "Hello #{name}!"
+  end
 end
--- hello_world/test/hello_world_test.exs
+++ hello_world/test/hello_world_test.exs
@@ -1,7 +1,11 @@
 defmodule HelloWorldTest do
   use ExUnit.Case
 
-  test "greets the world" do
+  test "greets the world when no name is given" do
     assert HelloWorld.hello() === "Hello World!"
+  end
+
+  test "greets a person by name when a name is given" do
+    assert HelloWorld.hello("George") === "Hello George!"
   end
 end
